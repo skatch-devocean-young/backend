f099711c19d3aecbd7a2cd1d6690ff01a4a378e6
bbbang105 <2018111366@dgu.ac.kr>
2024-07-24 17:56:15 +0900

    Initial Commit

---
+++ src/main/java/devocean/tickit/TickitApplication.java
@@ -0,0 +1,13 @@
+package devocean.tickit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TickitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/devocean/tickit/TickitApplicationTests.java
@@ -0,0 +1,13 @@
+package devocean.tickit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
