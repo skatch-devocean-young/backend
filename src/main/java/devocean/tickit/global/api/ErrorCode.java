5e42723eac94c4ed0e8e451a1fc8d1f77dbe6378
Ganghee-Lee-0522 <gh_5226@ewhain.net>
2024-08-18 01:57:45 +0900

    #9 [feat] : ErrorCode(사용자 정의 예외) enum 생성

---
+++ src/main/java/devocean/tickit/global/api/ErrorCode.java
@@ -0,0 +1,27 @@
+package devocean.tickit.global.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+    // 000 분류가 필요한 에러
+    // 00
+    UNCUSTOMED_ERROR(00000, HttpStatus.OK, "서버 에러 커스텀이 필요"),
+
+    // 404 Not Found
+    // 00
+    NOT_FOUND(40400, HttpStatus.NOT_FOUND, "존재하지 않음"),
+    NOT_FOUND_END_POINT(40401, HttpStatus.NOT_FOUND, "존재하지 않는 API"),
+
+    // 500 Internal Server Error
+    // 00
+    INTERBAL_SERVER_ERROR(50000, HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류"),
+
+    private final Integer code;
+    private final HttpStatus httpStatus;
+    private final String message;
+}
