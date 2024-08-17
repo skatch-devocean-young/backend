4f3bbc282ac365dcf45aae9034044ab7ce55cc39
Ganghee-Lee-0522 <gh_5226@ewhain.net>
2024-08-17 23:23:10 +0900

    #4 [fix] : @Value import 경로 변경

--- src/main/java/devocean/tickit/global/config/redis/RedisConfig.java
+++ src/main/java/devocean/tickit/global/config/redis/RedisConfig.java
@@ -1,6 +1,6 @@
 package devocean.tickit.global.config.redis;
 
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
