099419d9bda48d79e1e808f2a95ee6f8b8264e08
Ganghee-Lee-0522 <gh_5226@ewhain.net>
2024-08-18 23:19:24 +0900

    #4 [feat] : created RedisController.class

---
+++ src/main/java/devocean/tickit/global/config/redis/RedisController.java
@@ -0,0 +1,33 @@
+package devocean.tickit.global.config.redis;
+
+import devocean.tickit.global.api.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class RedisController {
+
+    private RedisTemplate<String, String> redisTemplate;
+
+    @PostMapping("/redisTest")
+    public ApiResponse<?> addRedisKey() {
+        ValueOperations<String, String> ops = redisTemplate.opsForValue();
+        ops.set("key1", "value1");
+        ops.set("key2", "value2");
+        ops.set("key3", "value3");
+        return ApiResponse.created(null);
+    }
+
+    @GetMapping("/redisTest/{key}")
+    public ApiResponse<?> getRedisKey(@PathVariable String key) {
+        ValueOperations<String, String> ops = redisTemplate.opsForValue();
+        String value = ops.get(key);
+        return ApiResponse.ok(value);
+    }
+}
