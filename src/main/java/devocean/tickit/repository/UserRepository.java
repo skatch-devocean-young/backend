3d428fb87714f6bda1b0d7cd436a767d5d8e4f29
Ganghee-Lee-0522 <gh_5226@ewhain.net>
2024-08-20 10:05:24 +0900

    #4 [feat] : add Query statement at UserRepository

--- src/main/java/devocean/tickit/repository/UserRepository.java
+++ src/main/java/devocean/tickit/repository/UserRepository.java
@@ -2,6 +2,13 @@ package devocean.tickit.repository;
 
 import devocean.tickit.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByProviderId(String providerId);
+
+    @Query("SELECT u FROM User u WHERE u.provider = ?1 AND u.providerId = ?2")
+    Optional<User> findByProviderAndProviderId(String provider, String providerId);
 }
