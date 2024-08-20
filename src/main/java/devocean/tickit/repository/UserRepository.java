790053e6aecd8f4ec8e05ce3f190502c8112124a
Ganghee-Lee-0522 <gh_5226@ewhain.net>
2024-08-20 10:45:06 +0900

    #4 [feat] : add AuthUtils

--- src/main/java/devocean/tickit/repository/UserRepository.java
+++ src/main/java/devocean/tickit/repository/UserRepository.java
@@ -5,8 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
+import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByProviderId(String providerId);
 
     @Query("SELECT u FROM User u WHERE u.provider = ?1 AND u.providerId = ?2")
