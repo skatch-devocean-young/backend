802f75c817799466a1f4e6c353f9eb014a762b58
Ganghee-Lee-0522 <gh_5226@ewhain.net>
2024-08-23 16:05:56 +0900

    [fix] : jwt filter 세팅

--- src/main/java/devocean/tickit/global/config/SecurityConfig.java
+++ src/main/java/devocean/tickit/global/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package devocean.tickit.global.config;
 
+import devocean.tickit.global.jwt.JwtFilter;
+import devocean.tickit.global.jwt.JwtUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,15 +10,43 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final JwtUtils jwtUtils;
+    private String apiVersion = "/api/v1";
+    private String[] attendeeList = {
+            "/", "/**",
+            "/health",
+            "/test", "/test/**",
+            "/user", "/users/**",
+            "/events", "/events/**",
+            "/wallets", "/wallets/**",
+            "/albums", "/albums/**",
+            "/tickets", "/tickets/**",
+            "/mypage", "/mypage/**",
+
+    };
+
+    private String[] hostList = {
+            "/participants", "/participants/**",
+    };
+
+    public String[] getVersionedList(String[] apiList) {
+        return Arrays.stream(apiList)
+                .map(path -> apiVersion + path)
+                .toArray(String[]::new);
+    }
+
 
     // CORS 설정
     CorsConfigurationSource corsConfigurationSource() {
@@ -31,16 +61,22 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.
                 httpBasic(HttpBasicConfigurer::disable)
                 .cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfigurationSource())) // CORS 설정 추가
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authorize ->
-                        authorize
-                                .requestMatchers("/**").permitAll()
-                );
+                .formLogin(AbstractHttpConfigurer::disable)
+                .sessionManagement(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((auth) -> {
+                    auth
+                            .requestMatchers(getVersionedList(attendeeList)).permitAll()
+                            .requestMatchers(getVersionedList(hostList)).permitAll()
+                            .anyRequest().authenticated();
+                })
+                .addFilterBefore(new JwtFilter(jwtUtils), UsernamePasswordAuthenticationFilter.class)
 
-        return httpSecurity.build();
+        ;
+        return http.build();
     }
 }
