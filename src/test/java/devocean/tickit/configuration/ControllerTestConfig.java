f0ea69755810dd20d50e98697d69f7764c023f63
bbbang105 <2018111366@dgu.ac.kr>
2024-11-13 02:56:20 +0900

    #37 [feat] : 이미지 Mock 파일을 만드는 메서드를 추가한다

--- src/test/java/devocean/tickit/configuration/ControllerTestConfig.java
+++ src/test/java/devocean/tickit/configuration/ControllerTestConfig.java
@@ -6,12 +6,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
+
+import java.nio.charset.StandardCharsets;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -26,6 +29,10 @@ public abstract class ControllerTestConfig {
     @Autowired
     protected ObjectMapper objectMapper;
     protected MockMvc mockMvc;
+
+    protected MockMultipartFile createMockImageFile() {
+        return new MockMultipartFile("imageFile", "test.jpg", "multipart/form-data", "test.jpg".getBytes(StandardCharsets.UTF_8));
+    }
 
     @BeforeEach
     void setUp(final RestDocumentationContextProvider restDocumentation) {
