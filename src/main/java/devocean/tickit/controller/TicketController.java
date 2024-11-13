054e07d1325242cef2aae036db1cf23226de6b06
Ganghee-Lee-0522 <gh_5226@ewhain.net>
2024-11-13 14:43:28 +0900

    feat: add logics to get ticket infos & list

---
+++ src/main/java/devocean/tickit/controller/TicketController.java
@@ -0,0 +1,27 @@
+package devocean.tickit.controller;
+
+import devocean.tickit.dto.ticket.request.TicketRequest;
+import devocean.tickit.global.api.ApiResponse;
+import devocean.tickit.service.TicketService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/api/v1/ticket")
+@RequiredArgsConstructor
+@RestController
+public class TicketController {
+
+    private final TicketService ticketService;
+
+    // 티켓 리스트 조회
+    @GetMapping("/list")
+    public ApiResponse<?> getTicketList(@RequestBody TicketRequest request) throws Exception {
+        return ticketService.getList(request);
+    }
+
+    // 개별 티켓 조회
+    @GetMapping("/{attendee_id}")
+    public ApiResponse<?> getTicketInfo(@PathVariable("attendee_id") Long attendeeId) throws Exception {
+        return ticketService.getTicket(attendeeId);
+    }
+}
